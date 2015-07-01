71757bd1801aeb5275fe89ffc16d2351d3ff28d3
Matthew Villaflores <jm_07villaflores@yahoo.com>
2015-07-01 17:18:31 +0800

    DrawerNavigation on Main Activity

--- app/src/main/java/com/tech/ivant/qapp/MainActivity.java
+++ app/src/main/java/com/tech/ivant/qapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.tech.ivant.qapp;
 
+import android.support.v7.app.ActionBar;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,9 +18,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity{
 
     public final static String SERVICE_ID_EXTRA = "com.tech.ivant.service_id";
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private CharSequence mTitle;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,40 @@ public class MainActivity extends ActionBarActivity {
 
         DBManager.initializeDB(this);
 
+        mDrawerList = (ListView)findViewById(R.id.navigationDrawer);
+        String[] drawer_list = {
+                getString(R.string.nav_monitor_queue_title),
+                getString(R.string.nav_sms_title),
+                getString(R.string.nav_reports_title),
+                getString(R.string.nav_settings_title)};
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drawer_list);
+        mDrawerList.setAdapter(mAdapter);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mActivityTitle = getTitle().toString();
+        setUpDrawer();
+
+    }
+
+    private void setUpDrawer(){
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     @Override
@@ -47,22 +93,34 @@ public class MainActivity extends ActionBarActivity {
         final MainActivity context = this;
 
         servicesList.setClickable(true);
-        servicesList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        servicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent open_service = new Intent(context, ViewService.class);
-                open_service.putExtra(SERVICE_ID_EXTRA, services[position].id+"");
+                open_service.putExtra(SERVICE_ID_EXTRA, services[position].id + "");
                 startActivity(open_service);
             }
         });
 
     }
 
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //if(!mNavigationDrawerFragment.isDrawerOpen()){
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+        //    restoreActionBar();
+        //    return true;
+        //}
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -70,18 +128,35 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
+        //int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
     public void newService(View view){
         Intent intent = new Intent(this, NewService.class);
         startActivity(intent);
     }
+
 }
