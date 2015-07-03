1b850436168cc4024a038b26f47abac589a7fb4f
Matthew Villaflores <jm_07villaflores@yahoo.com>
2015-07-03 09:34:16 +0800

    v2

--- app/src/main/java/com/tech/ivant/qapp/MainActivity.java
+++ app/src/main/java/com/tech/ivant/qapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tech.ivant.qapp;
 
 import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,10 +16,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.tech.ivant.qapp.fragments.MonitorQueueFragment;
+
 
 public class MainActivity extends ActionBarActivity {
 
     public final static String SERVICE_ID_EXTRA = "com.tech.ivant.service_id";
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +34,68 @@ public class MainActivity extends ActionBarActivity {
 
         DBManager.initializeDB(this);
 
+        mDrawerList = (ListView)findViewById(R.id.navigationDrawer);
+        String[] drawer_list = {
+                getString(R.string.nav_monitor_queue_title),
+                getString(R.string.nav_sms_title),
+                getString(R.string.nav_reports_title),
+                getString(R.string.nav_settings_title)};
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drawer_list);
+        mDrawerList.setAdapter(mAdapter);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mActivityTitle = getTitle().toString();
+        setUpDrawer();
+
+        Fragment home_fragment = new MonitorQueueFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_container, home_fragment).commit();
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        });
+
+    }
+
+    public void selectItem(int position){
+        Fragment fragment;
+        switch(position){
+            case 1:
+//                fragment = new SmsFragment();
+                break;
+            case 2:
+//                fragment = new ReportFragment();
+                break;
+            case 3:
+//                fragment = new SettingsFragment();
+                break;
+            default:
+                fragment = new MonitorQueueFragment();
+                break;
+        }
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frame_container, fragment).commit();
+
+        mDrawerLayout.closeDrawer(mDrawerList);
+
+    }
     }
 
     @Override
     public void onResume(){
         super.onResume();
-
+/*8
         final Service[] services = Service.all(this);
 
         final ListView servicesList = (ListView) findViewById(R.id.serviceListView);
@@ -55,7 +120,7 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(open_service);
             }
         });
-
+*/
     }
 
     @Override
