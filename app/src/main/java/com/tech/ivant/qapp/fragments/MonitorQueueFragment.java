7658587ac94e7c24cf6738cf1198d0b9d195043b
Matthew Villaflores <jm_07villaflores@yahoo.com>
2015-07-14 17:26:43 +0800

    So far ok, tried using horizontal scroll view with buttons

--- app/src/main/java/com/tech/ivant/qapp/fragments/MonitorQueueFragment.java
+++ app/src/main/java/com/tech/ivant/qapp/fragments/MonitorQueueFragment.java
@@ -5,14 +5,19 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.tech.ivant.qapp.MainActivity;
+import com.tech.ivant.qapp.Queue;
 import com.tech.ivant.qapp.R;
 import com.tech.ivant.qapp.Service;
 import com.tech.ivant.qapp.ViewService;
@@ -21,31 +26,56 @@ import com.tech.ivant.qapp.ViewService;
  * Created by matthew on 7/3/15.
  */
 public class MonitorQueueFragment extends Fragment {
+
+    private Service mService;
+
     public MonitorQueueFragment(){}
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.fragment_monitor_queue, container, false);
+        //updateList(rootView);
+
+        Service services[] = Service.all(getActivity());
+
+        mService = services[0];
+
+        HorizontalScrollView hScroll = (HorizontalScrollView) rootView.findViewById(R.id.serviceListHScroll);
+        LinearLayout linearLayout = (LinearLayout) rootView.findViewById(R.id.serviceListLinearLayout);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.gravity = Gravity.CENTER;
+        for(Service service : services){
+            Button button = new Button(getActivity());
+            button.setText(service.name);
+
+            linearLayout.addView(button, layoutParams);
+        }
+
         updateList(rootView);
+
         return rootView;
     }
 
 
     public void updateList(View view){
 
-        final Service[] services = Service.all(this.getActivity());
+        final Queue[] queues = Queue.where(getActivity(), Queue.QueueEntry.COLUMN_NAME_SERVICE_ID, mService.id+"");
 
-        final ListView servicesList = (ListView) view.findViewById(R.id.serviceListView);
+        final ListView servicesList = (ListView) view.findViewById(R.id.monitorQueueQueueList);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1);
         servicesList.setAdapter(adapter);
 
-        if( services != null) {
-            for (Service service : services) {
-                adapter.add(service.name);
+        if( queues != null) {
+            for (Queue queue : queues) {
+                adapter.add(queue.customerName);
             }
         }
-
+/*
         final Activity context = this.getActivity();
 
         servicesList.setClickable(true);
@@ -57,6 +87,7 @@ public class MonitorQueueFragment extends Fragment {
                 startActivity(open_service);
             }
         });
-
+*/
     }
+
 }
